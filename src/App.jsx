67c55d741f80cf7398cc684c1dd9e07a456c6e2e
oscar <oscar.delfos369.com>
2023-09-18 16:25:54 -0600

    Login Facebook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import LoginFacebook from './pages/LoginFacebook'
 
 function App() {
 
   return (
     <>
-
+    <LoginFacebook/>
     </>
   )
 }
---
+++ src/pages/LoginFacebook.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import "./login-facebook.css"
+import Logo from "../assets/logoFacebook.svg";
+
+function LoginFacebook() {
+
+    return (
+        <div className='login-facebook'>
+
+            <div className='logo-facebook'>
+                <img src={Logo} />
+                <h2>Connect with friends and the world around you on Facebook.
+                </h2>
+            </div>
+
+            <div className='card-facebook'>
+
+                <div className='data-facebook'>
+                    <input placeholder='Email or Phone number' />
+                    <input placeholder='Password' />
+                </div>
+
+                <div className='btn-facebook'>
+                    <button>Log In</button>
+                </div>
+
+                <div className='changePassword'>
+                    <a>Forgot password?</a>
+                </div>
+
+                <div className='new-account'>
+                    <button>Create New Account</button>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default LoginFacebook
