8b1bb5fcc0e1acc1d0ce43f4550ef24b07757ed6
oscar <oscar.delfos369.com>
2023-10-11 03:29:27 -0600

    Login glass v3

--- src/pages/LoginGlass.jsx
+++ src/pages/LoginGlass.jsx
@@ -5,18 +5,7 @@ function LoginGlass() {
     return (
         <div className='login'>
             <div className='card-box'>
-                <div className='information'>
-                    <h1>Logo</h1>
-                    <h2>Welcome</h2>
-                    <h3>to Our New Website</h3>
-                    <p>Lorem ipsum,dolor sit amen consenter adipisicing elit. Beatae, asperiores</p>
-                    <div className='social-media'>
-                        <a><i className="bi bi-instagram"></i></a>
-                        <a><i className="bi bi-facebook"></i></a>
-                        <a><i className="bi bi-github"></i></a>
-                    </div>
-                    <span>oscardigital.website</span>
-                </div>
+            
                 <div className='form'>
                     <h2>Login</h2>
                     <div className='data'>
@@ -38,6 +27,13 @@ function LoginGlass() {
                     <div className='register'>
                         <span>Don´t have a account?</span> <a>Sign up</a>
                     </div>
+
+                    <div className='social-media'>
+                        <a><i className="bi bi-instagram"></i></a>
+                        <a><i className="bi bi-facebook"></i></a>
+                        <a><i className="bi bi-github"></i></a>
+                    </div>
+                    <span>oscardigital.website</span>
                 </div>
             </div>
         </div>
