d6bad6b85f506a300b6c596eab63529d3ad4d2d3
oscar <oscar2905@gmail.com>
2023-12-07 01:52:48 -0600

    style add

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,11 +1,91 @@
 import React from 'react'
 import "./style.css"
 
+const IconClock = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" className="bi bi-clock-history" viewBox="0 0 16 16">
+    <path d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022zm2.004.45a7.003 7.003 0 0 0-.985-.299l.219-.976c.383.086.76.2 1.126.342zm1.37.71a7.01 7.01 0 0 0-.439-.27l.493-.87a8.025 8.025 0 0 1 .979.654l-.615.789a6.996 6.996 0 0 0-.418-.302zm1.834 1.79a6.99 6.99 0 0 0-.653-.796l.724-.69c.27.285.52.59.747.91l-.818.576zm.744 1.352a7.08 7.08 0 0 0-.214-.468l.893-.45a7.976 7.976 0 0 1 .45 1.088l-.95.313a7.023 7.023 0 0 0-.179-.483m.53 2.507a6.991 6.991 0 0 0-.1-1.025l.985-.17c.067.386.106.778.116 1.17l-1 .025zm-.131 1.538c.033-.17.06-.339.081-.51l.993.123a7.957 7.957 0 0 1-.23 1.155l-.964-.267c.046-.165.086-.332.12-.501zm-.952 2.379c.184-.29.346-.594.486-.908l.914.405c-.16.36-.345.706-.555 1.038l-.845-.535m-.964 1.205c.122-.122.239-.248.35-.378l.758.653a8.073 8.073 0 0 1-.401.432l-.707-.707z" />
+    <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0z" />
+    <path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5" />
+  </svg>
+)
+
+const IconNext = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+    <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z" />
+  </svg>
+)
+
+const IconBefore = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+    <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z" />
+  </svg>
+)
+
+const IconPlay = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-play-fill" viewBox="0 0 16 16">
+    <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
+  </svg>
+)
+
 function Card() {
   return (
-   <section>
+    <section className='container'>
 
-   </section>
+      <div className="card">
+
+        <div className="item">
+          <h2>Mesage</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipiscing elit nec varius hendrerit ornare magna gravida in, maecenas lacus etiam litora morbi a aliquam pellentesque volutpat cursus vitae lacinia torquent. </p>
+        </div>
+
+        <div className="item">
+          <span>86,3%</span>
+          <div className='progress'></div>
+        </div>
+
+        <div className="item">
+          <div className='circle'>
+            15%
+          </div>
+        </div>
+
+        <div className="item">
+          <p>name of music</p>
+          <div className='player'>
+            <button><IconBefore /></button>
+            <button><IconPlay /></button>
+            <button><IconNext /></button>
+          </div>
+        </div>
+
+        <div className="item">
+          <span><IconClock /> </span>
+          <p>12:00 PM</p>
+        </div>
+
+        <div className="item">
+          <img src="https://images.pexels.com/photos/1323206/pexels-photo-1323206.jpeg?cs=srgb&dl=pexels-mixu-1323206.jpg&fm=jpg" />
+          <p>Name of Music</p>
+        </div>
+
+        <div className="item">
+          <ol>
+            <li>Song 1</li>
+            <li>Song 2</li>
+            <li>Song 3</li>
+            <li>Song 4</li>
+            <li>Song 5</li>
+            <li>Song 6</li>
+          </ol>
+        </div>
+
+        <div className="item">
+          <img src="https://images.pexels.com/photos/268941/pexels-photo-268941.jpeg?cs=srgb&dl=pexels-pixabay-268941.jpg&fm=jpg"  />
+          <p>Name of music</p>
+        </div>
+
+      </div>
+    </section>
   )
 }
 
