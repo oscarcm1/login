b8f2c563e238615655f67d845d5cb271282b0ba6
oscar <oscar2905@gmail.com>
2024-01-08 04:01:37 -0600

    login add style

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,35 +1,35 @@
 import React from 'react'
 import "./style.css"
-import {GoogleIcon , FacebookIcon } from "./Icons"
+import { GoogleIcon, FacebookIcon } from "./Icons"
 
 function Card() {
   return (
+    <div className='container'>
 
-    <section className='card'>
-       <h1>Logo</h1>
 
-      <div className='circle'>
-        <div className="circle2"></div>
-      </div>
-
-      <section className='card-login'>
-        <form action="" className='login'>
-          <h2>Sign in</h2>
-          <span className=''>New user?  <a href="#">Create an Account</a></span>
-          <input type="text" placeholder='Email Adress' />
-          <input type="text" placeholder='Password' />
-          <button>Login</button>
-        </form>
-
-        <span>Or</span>
-
-        <section className='social-media'>
-          <button> <GoogleIcon/>   Google</button>
-          <button> <FacebookIcon/> Facebook</button>
+      <section className='card'>
+        <div className='circle'>
+          <div className="circle2"></div>
+        </div>
+        <h1>Logo</h1>
+        <section className='card-login'>
+          <form action="" className='login'>
+            <h2>Sign in</h2>
+            <span className=''>New user?  <a href="#">Create an Account</a></span>
+            <input type="text" placeholder='Email Adress' />
+            <input type="text" placeholder='Password' />
+            <button>Login</button>
+          </form>
+          <span>Or</span>
+          <section className='social-media'>
+            <button> <GoogleIcon />   Google</button>
+            <button> <FacebookIcon /> Facebook</button>
+          </section>
         </section>
       </section>
 
-    </section>
+    </div>
+
   )
 }
 
